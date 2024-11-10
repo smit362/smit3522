37a5ae7397e384d1c43b8ac417d725211cdfbc8a
smit362 <sp332103@gmail.com>
2024-11-11 00:13:38 +0530

    Add files via upload

---
+++ grocery/js/script.js
@@ -0,0 +1,6 @@
+Let searchform = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>
+{
+    searchForm.classList.toggle('active');
+}
---
+++ grocery/main.py
@@ -0,0 +1,6 @@
+import phonenumders
+form test import numbers
+
+form phonenumders import geocoder
+ch_number = phonenumders.parse( number, "ch")
+print(geocoder.description_for_number(ch_number, "en"))
